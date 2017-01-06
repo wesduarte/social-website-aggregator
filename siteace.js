b0f44b092d0c96099a11d6987e708d55b908e50f
wesley <wesley@wesleys-MacBook-Pro.local>
2017-01-06 02:31:52 -0200

    Sorting websites by most upvoted site

--- siteace.js
+++ siteace.js
@@ -20,7 +20,7 @@ if (Meteor.isClient) {
 	// helper function that returns all available websites
 	Template.website_list.helpers({
 		websites:function(){
-			return Websites.find({});
+			return Websites.find({},{sort: { upvotes: -1}});
 		}
 	});
 
