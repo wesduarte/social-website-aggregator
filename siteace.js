0d95959c7076dee8a39cacbc54ceab5fc269b0f5
wesley <wesley@wesleys-MacBook-Pro.local>
2017-01-06 01:21:04 -0200

    Adding user accounts

--- siteace.js
+++ siteace.js
@@ -2,8 +2,19 @@ Websites = new Mongo.Collection("websites");
 
 if (Meteor.isClient) {
 
+	Accounts.ui.config({
+	  requestPermissions: {
+	    facebook: ['user_likes'],
+	    github: ['user', 'repo']
+	  },
+	  requestOfflineToken: {
+	    google: true
+	  },
+	  passwordSignupFields: 'USERNAME_AND_OPTIONAL_EMAIL'
+	});
+
 	/////
-	// template helpers 
+	// template helpers
 	/////
 
 	// helper function that returns all available websites
@@ -15,7 +26,7 @@ if (Meteor.isClient) {
 
 
 	/////
-	// template events 
+	// template events
 	/////
 
 	Template.website_item.events({
@@ -27,7 +38,7 @@ if (Meteor.isClient) {
 			// put the code in here to add a vote to a website!
 
 			return false;// prevent the button from reloading the page
-		}, 
+		},
 		"click .js-downvote":function(event){
 
 			// example of how you can access the id for the website in the database
@@ -44,14 +55,14 @@ if (Meteor.isClient) {
 	Template.website_form.events({
 		"click .js-toggle-website-form":function(event){
 			$("#website_form").toggle('slow');
-		}, 
+		},
 		"submit .js-save-website-form":function(event){
 
 			// here is an example of how to get the url out of the form:
 			var url = event.target.url.value;
 			console.log("The url they entered is: "+url);
-			
-			//  put your website saving code in here!	
+
+			//  put your website saving code in here!
 
 			return false;// stop the form submit from reloading the page
 
@@ -67,27 +78,27 @@ if (Meteor.isServer) {
     if (!Websites.findOne()){
     	console.log("No websites yet. Creating starter data.");
     	  Websites.insert({
-    		title:"Goldsmiths Computing Department", 
-    		url:"http://www.gold.ac.uk/computing/", 
-    		description:"This is where this course was developed.", 
+    		title:"Goldsmiths Computing Department",
+    		url:"http://www.gold.ac.uk/computing/",
+    		description:"This is where this course was developed.",
     		createdOn:new Date()
     	});
     	 Websites.insert({
-    		title:"University of London", 
-    		url:"http://www.londoninternational.ac.uk/courses/undergraduate/goldsmiths/bsc-creative-computing-bsc-diploma-work-entry-route", 
-    		description:"University of London International Programme.", 
+    		title:"University of London",
+    		url:"http://www.londoninternational.ac.uk/courses/undergraduate/goldsmiths/bsc-creative-computing-bsc-diploma-work-entry-route",
+    		description:"University of London International Programme.",
     		createdOn:new Date()
     	});
     	 Websites.insert({
-    		title:"Coursera", 
-    		url:"http://www.coursera.org", 
-    		description:"Universal access to the world’s best education.", 
+    		title:"Coursera",
+    		url:"http://www.coursera.org",
+    		description:"Universal access to the world’s best education.",
     		createdOn:new Date()
     	});
     	Websites.insert({
-    		title:"Google", 
-    		url:"http://www.google.com", 
-    		description:"Popular search engine.", 
+    		title:"Google",
+    		url:"http://www.google.com",
+    		description:"Popular search engine.",
     		createdOn:new Date()
     	});
     }
