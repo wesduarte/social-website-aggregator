d5a875923f654d5297c056f4ee55400c766f0267
wesley <wesley@wesleys-MacBook-Pro.local>
2017-01-06 02:25:16 -0200

    Users can up and down vote webpages by clicking a plus or a minus button

--- siteace.js
+++ siteace.js
@@ -35,6 +35,10 @@ if (Meteor.isClient) {
 			// (this is the data context for the template)
 			var website_id = this._id;
 			console.log("Up voting website with id "+website_id);
+
+			Websites.update(website_id,
+				{$inc: {upvotes: 1}}
+			);
 			// put the code in here to add a vote to a website!
 
 			return false;// prevent the button from reloading the page
@@ -45,6 +49,10 @@ if (Meteor.isClient) {
 			// (this is the data context for the template)
 			var website_id = this._id;
 			console.log("Down voting website with id "+website_id);
+
+			Websites.update(website_id,
+				{$inc: {downvotes: 1}}
+			);
 
 			// put the code in here to remove a vote from a website!
 
@@ -57,7 +65,7 @@ if (Meteor.isClient) {
 			$("#website_form").toggle('slow');
 		},
 		"submit .js-save-website-form":function(event){
-
+			event.preventDefault();
 			// here is an example of how to get the url out of the form:
 			var target = event.target;
 			console.log("My target: "+target);
@@ -72,7 +80,9 @@ if (Meteor.isClient) {
 			 title:title,
 			 url:url,
 			 description:description,
-			 createdOn:new Date()
+			 createdOn:new Date(),
+			 upvotes: 0,
+			 downvotes: 0,
 		 });
 
 			return false;// stop the form submit from reloading the page
@@ -92,25 +102,33 @@ if (Meteor.isServer) {
     		title:"Goldsmiths Computing Department",
     		url:"http://www.gold.ac.uk/computing/",
     		description:"This is where this course was developed.",
-    		createdOn:new Date()
+    		createdOn:new Date(),
+				upvotes: 0,
+				downvotes: 0,
     	});
     	 Websites.insert({
     		title:"University of London",
     		url:"http://www.londoninternational.ac.uk/courses/undergraduate/goldsmiths/bsc-creative-computing-bsc-diploma-work-entry-route",
     		description:"University of London International Programme.",
-    		createdOn:new Date()
+    		createdOn:new Date(),
+				upvotes: 0,
+				downvotes: 0,
     	});
     	 Websites.insert({
     		title:"Coursera",
     		url:"http://www.coursera.org",
     		description:"Universal access to the world’s best education.",
-    		createdOn:new Date()
+    		createdOn:new Date(),
+				upvotes: 0,
+				downvotes: 0,
     	});
     	Websites.insert({
     		title:"Google",
     		url:"http://www.google.com",
     		description:"Popular search engine.",
-    		createdOn:new Date()
+    		createdOn:new Date(),
+				upvotes: 0,
+				downvotes: 0,
     	});
     }
   });
