b932bd6015e0a51503a28de863429c0b1cc06212
Wesley <wesleydmuniz@gmail.com>
2017-03-19 00:37:41 -0300

    Refacting some codes and fixing some bugs

--- client/main.js
+++ client/main.js
@@ -6,176 +6,164 @@ Router.configure({
     layoutTemplate: 'main'
 });
 
-	Router.route('/', {
-		template:'website_list_page'
-	});
+Router.route('/', {
+	template:'website_list_page'
+});
 
-	Router.route('/details/:_id', {
-		template: 'website_details',
-		data: function() {
-				let website_id = this.params._id;
-				return Websites.findOne({_id: website_id});
+Router.route('/details/:_id', {
+	template: 'website_details',
+	data: function() {
+			let website_id = this.params._id;
+			return Websites.findOne({_id: website_id});
+		}
+});
+
+Accounts.ui.config({
+	 requestPermissions: {
+	   facebook: ['user_likes'],
+	   github: ['user', 'repo']
+	 },
+	 requestOfflineToken: {
+    google: true
+  },
+  passwordSignupFields: 'USERNAME_AND_OPTIONAL_EMAIL'
+});
+
+Meteor.subscribe("websites");
+Meteor.subscribe("comments");
+Meteor.subscribe("votes");
+
+Template.website_list.helpers({
+	websites:function(){
+		return Websites.find({}, {sort: {upvotes : -1, downvotes:1}});
+	}
+});
+
+Template.website_details.helpers({
+	comments:function(){
+		return Comments.find({website: this._id}, {sort: {createdOn: -1}});
+	}
+});
+
+Template.comment_item.helpers({
+	get_user:function(user_id){
+		let user = Meteor.users.findOne({_id: user_id});
+		return user.username;
+	}
+});
+
+Template.website_item.events({
+	"click .js-upvote":function(event){
+    if(Meteor.user()){
+		  let website_id = this._id;
+		  let user_id = Meteor.userId();
+			let vote_type = 1;
+			console.log("Up voting website with id "+website_id);
+			let vote = Votes.findOne({website: website_id, user: user_id});
+
+			if(vote){
+				Votes.update(vote._id, {website:website_id, user:user_id, vote_type:vote_type});
+			} else {
+				Votes.insert({website:website_id, user:user_id, vote_type:vote_type});
 			}
-		});
 
-	Accounts.ui.config({
-	  requestPermissions: {
-	    facebook: ['user_likes'],
-	    github: ['user', 'repo']
-	  },
-	  requestOfflineToken: {
-	    google: true
-	  },
-	  passwordSignupFields: 'USERNAME_AND_OPTIONAL_EMAIL'
-	});
+      upvotes = Votes.find({website: this._id, vote_type: 1}).count()
+      downvotes = Votes.find({website: this._id, vote_type: 2}).count()
+      Websites.update(website_id,{
+        $set: {upvotes: upvotes, downvotes: downvotes}
+      });
 
-  Meteor.subscribe("websites");
-  Meteor.subscribe("comments");
-  Meteor.subscribe("votes");
-
-	Template.website_list.helpers({
-		websites:function(){
-			return Websites.find({}, {sort: {upvotes : -1, downvotes:1}});
-		}
-	});
-
-	Template.website_details.helpers({
-		comments:function(){
-			return Comments.find({website: this._id}, {sort: {createdOn: -1}});
-		}
-	});
-
-	Template.comment_item.helpers({
-		get_user:function(user_id){
-			let user = Meteor.users.findOne({_id: user_id});
-			return user.username;
-		}
-	});
-
-	Template.website_item.events({
-		"click .js-upvote":function(event){
-
-      if(Meteor.user()){
-			   let website_id = this._id;
-			   let user_id = Meteor.userId();
-			   let vote_type = 1;
-			   console.log("Up voting website with id "+website_id);
-
-			   let vote = Votes.findOne({website: website_id, user: user_id});
-
-			   if(vote){
-				    Votes.update(vote._id, {website:website_id, user:user_id, vote_type:vote_type});
-			   } else {
-				    Votes.insert({website:website_id, user:user_id, vote_type:vote_type});
-			   }
-
-         upvotes = Votes.find({website: this._id, vote_type: 1}).count()
-         downvotes = Votes.find({website: this._id, vote_type: 2}).count()
-         Websites.update(website_id,{
-           $set: {upvotes: upvotes, downvotes: downvotes}
-         });
-
-         $().button('toggle')
-
+      $().button('toggle')
       }
-
 			return false;
 		},
-		"click .js-downvote":function(event){
+	"click .js-downvote":function(event){
+    if(Meteor.user()){
+		  let website_id = this._id;
+			let user_id = Meteor.userId();
+			let vote_type = 2;
+			console.log("Down voting website with id "+website_id);
+			var vote = Votes.findOne({website: website_id, user: user_id});
 
-      if(Meteor.user()){
-			   let website_id = this._id;
-			   let user_id = Meteor.userId();
-			   let vote_type = 2;
-			   console.log("Down voting website with id "+website_id);
+			if(vote){
+		    Votes.update({_id: vote._id}, { website:website_id, user:user_id, vote_type:vote_type});
+		  } else {
+		    Votes.insert({website:website_id, user:user_id, vote_type:vote_type});
+	    }
 
-			   var vote = Votes.findOne({website: website_id, user: user_id});
+      upvotes = Votes.find({website: this._id, vote_type: 1}).count()
+      downvotes = Votes.find({website: this._id, vote_type: 2}).count()
+      Websites.update(website_id,{
+        $set: {upvotes: upvotes, downvotes: downvotes}
+      });
 
-			   if(vote){
-				    Votes.update({_id: vote._id}, { website:website_id, user:user_id, vote_type:vote_type});
-			   } else {
-				    Votes.insert({website:website_id, user:user_id, vote_type:vote_type});
-			   }
-
-         upvotes = Votes.find({website: this._id, vote_type: 1}).count()
-         downvotes = Votes.find({website: this._id, vote_type: 2}).count()
-         Websites.update(website_id,{
-           $set: {upvotes: upvotes, downvotes: downvotes}
-         });
-
-         $().button('toggle')
+      $().button('toggle')
       }
-
 			return false;
-		}
-	})
+	 }
+ });
 
-	Template.registerHelper('date', function(){
+Template.registerHelper('date', function(){
+  if(!jQuery.isEmptyObject(this)){
+	   let obj_id = this._id;
+     if(typeof this.url == "undefined")
+		   var obj = Comments.findOne({_id: obj_id});
+     else {
+       var obj = Websites.findOne({_id: obj_id});
+     }
 
-		if(!jQuery.isEmptyObject(this)){
-			let obj_id = this._id;
-      if(typeof this.url == "undefined")
-			   var obj = Comments.findOne({_id: obj_id});
-      else {
-         var obj = Websites.findOne({_id: obj_id});
-      }
+     let month_index = obj.createdOn.getMonth();
+     let month = get_months()[month_index];
+     let day = obj.createdOn.getUTCDate();
+     let year = obj.createdOn.getFullYear();
+     let date = month + ", " + day + ", " + year;
 
-      let month_index = obj.createdOn.getMonth();
-      let month = get_months()[month_index];
-      let day = obj.createdOn.getUTCDate();
-      let year = obj.createdOn.getFullYear();
-      let date = month + ", " + day + ", " + year;
+		 return date;
+	}
+});
 
-			return date;
-		}
-	});
+Template.website_form.events({
+  "click .js-toggle-website-form":function(event){
+	   $("#website_url_form").toggle('slow');
+	},
+	"submit .js-save-website-form":function(event, template){
+	   event.preventDefault();
+		 let target = event.target;
+		 let url = target.url.value;
+		 let title = target.title.value;
+		 let description = target.description.value;
+		 console.log("The url they entered is: "+url);
 
-	Template.website_form.events({
-		"click .js-toggle-website-form":function(event){
-			$("#website_url_form").toggle('slow');
-		},
-		"submit .js-save-website-form":function(event, template){
-			event.preventDefault();
-			let target = event.target;
-			let url = target.url.value;
-			let title = target.title.value;
-			let description = target.description.value;
-			console.log("The url they entered is: "+url);
+     if( title == "" || description == ""){
+       if(!url.match(/^[a-zA-Z]+:\/\//))
+         url = 'http://' + url;
+         Meteor.call('get_url_info', url, function(error, response){
+        if(error)
+          $("#website_form").toggle('slow');
+        else{
+          url = url.replace(/http:\/\/|https:\/\//gi, "");
+          let title = response.title;
+          let description = response.description;
+          if(response.title != "" && response.description != ""){
+            Websites.insert({
+        	     title:title,
+        	     url:url,
+               description:description,
+        			 createdOn:new Date(),
+               upvotes:0,
+               downvotes: 0,
+        		});
 
-      if( title == "" || description == ""){
-          if(!url.match(/^[a-zA-Z]+:\/\//))
-            url = 'http://' + url;
-
-          Meteor.call('get_url_info', url, function(error, response){
-            if(error)
-              $("#website_form").toggle('slow');
-            else{
-              url = url.replace(/http:\/\/|https:\/\//gi, "");
-              let title = response.title;
-              let description = response.description;
-              if(response.title != "" && response.description != ""){
-
-                Websites.insert({
-        			    title:title,
-        			    url:url,
-                  description:description,
-        			    createdOn:new Date(),
-                  upvotes:0,
-                  downvotes: 0,
-        		    });
-
-                template.find("form").reset();
-       			      $("#website_url_form").toggle('slow');
-
-              } else {
-                $("#website_form").toggle('slow');
-              }
-          }
+            template.find("form").reset();
+       			$("#website_url_form").toggle('slow');
+           } else {
+             $("#website_form").toggle('slow');
+           }
+         }
           return false;
-        });
-      } else{
-
-			 Websites.insert({
+      });
+    } else{
+	     Websites.insert({
 			    title:title,
 			    url:url,
 			    description:description,
@@ -191,26 +179,26 @@ Router.configure({
 		}
 	});
 
-	Template.comment_form.events({
-		"submit .js-save-comment-form":function(event, template){
-			event.preventDefault();
-			let target = event.target;
-			let website = this._id;
-			let user = Meteor.userId();
-			let comment = target.comment.value;
+Template.comment_form.events({
+  "submit .js-save-comment-form":function(event, template){
+	   event.preventDefault();
+		 let target = event.target;
+		 let website = this._id;
+		 let user = Meteor.userId();
+		 let comment = target.comment.value;
 
-			console.log("The comment they entered is: "+comment);
+		 console.log("The comment they entered is: "+comment);
 
-			Comments.insert({
+		 Comments.insert({
 			 comment: comment,
 			 createdOn:new Date(),
 			 user: user,
 			 website: website,
 		 });
-		 	template.find("form").reset();
-			return false;
-		}
-	});
+		 template.find("form").reset();
+		 return false;
+	}
+});
 
 function get_months(){
   months = new Array(12);
--- server/main.js
+++ server/main.js
@@ -3,46 +3,42 @@ import '/imports/api/comments.js';
 import '/imports/api/votes.js';
 
 Meteor.startup(function () {
-    if (!Websites.findOne()){
-    	console.log("No websites yet. Creating starter data.");
-    	  Websites.insert({
-    		title:"Goldsmiths Computing Department",
-    		url:"http://www.gold.ac.uk/computing/",
-    		description:"This is where this course was developed.",
-    		createdOn:new Date(),
-        upvotes:0,
-        downvotes: 0,
-
-    	});
-    	 Websites.insert({
-    		title:"University of London",
-    		url:"http://www.londoninternational.ac.uk/courses/undergraduate/goldsmiths/bsc-creative-computing-bsc-diploma-work-entry-route",
-    		description:"University of London International Programme.",
-    		createdOn:new Date(),
-        upvotes:0,
-        downvotes: 0,
-
-    	});
-    	 Websites.insert({
-    		title:"Coursera",
-    		url:"http://www.coursera.org",
-    		description:"Universal access to the world’s best education.",
-    		createdOn:new Date(),
-        upvotes:0,
-        downvotes: 0,
-
-    	});
-    	Websites.insert({
-    		title:"Google",
-    		url:"http://www.google.com",
-    		description:"Popular search engine.",
-    		createdOn:new Date(),
-        upvotes:0,
-        downvotes: 0,
-
-    	});
-    }
-  });
+  if (!Websites.findOne()){
+    console.log("No websites yet. Creating starter data.");
+    Websites.insert({
+      title:"Goldsmiths Computing Department",
+    	url:"https://www.gold.ac.uk/computing/",
+    	description:"This is where this course was developed.",
+    	createdOn:new Date(),
+      upvotes:0,
+      downvotes: 0,
+    });
+    Websites.insert({
+    	title:"University of London",
+    	url:"https://www.londoninternational.ac.uk/courses/undergraduate/goldsmiths/bsc-creative-computing-bsc-diploma-work-entry-route",
+    	description:"University of London International Programme.",
+    	createdOn:new Date(),
+      upvotes:0,
+      downvotes: 0,
+    });
+    Websites.insert({
+    	title:"Coursera",
+    	url:"https://www.coursera.org",
+    	description:"Universal access to the world’s best education.",
+    	createdOn:new Date(),
+      upvotes:0,
+      downvotes: 0,
+    });
+    Websites.insert({
+    	title:"Google",
+    	url:"http://www.google.com",
+    	description:"Popular search engine.",
+    	createdOn:new Date(),
+      upvotes:0,
+      downvotes: 0,
+    });
+  }
+});
 
 Meteor.publish("websites", function(){
   return Websites.find({});
@@ -56,25 +52,23 @@ Meteor.publish("votes", function(){
   return Votes.find({});
 });
 
-  Meteor.methods({
-    'get_url_info':function(url){
-      this.unblock();
+Meteor.methods({
+  'get_url_info':function(url){
+    this.unblock();
 
-      let content = HTTP.get(url).content;
-      let title = "";
-      let description = "";
+    let content = HTTP.get(url).content;
+    let title = "";
+    let description = "";
+    let titleRegExp = /<title.*?>([\w\W]+)<\/title>/g;
+    let descriptionRegExp = /<meta.+?name="description".+?content="(.+?)"/g;
+    let title_matchs = titleRegExp.exec(content);
+    let description_matchs = descriptionRegExp.exec(content);
 
-      let titleRegExp = /<title.*?>([\w\W]+)<\/title>/g
-      let descriptionRegExp = /<meta.+?name="description".+?content="(.+?)"/g;
-      let title_matchs = titleRegExp.exec(content);
-      let description_matchs = descriptionRegExp.exec(content);
+    if(title_matchs)
+      title = title_matchs[1];
+    if(description_matchs)
+      description = description_matchs[1];
 
-      if(title_matchs)
-        title = title_matchs[1];
-      if(description_matchs)
-        description = description_matchs[1];
-
-      return {title: title, description: description};
-
-    }
-  });
+    return {title: title, description: description};
+  }
+});
